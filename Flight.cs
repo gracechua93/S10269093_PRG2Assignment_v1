165059d3c0a6e247d39400bbe43c2e85e011ef81
gracechua93 <chua.faithboy@gmail.com>
2025-01-23 21:24:22 +0800

    added flight class

---
+++ Flight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Flight
+{
+	// properties
+	public string FlightNumber { get; set; }
+	public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTime ExpectedTime { get; set; } 
+    public string Status { get; set; }
+    
+    // constructors
+    public Flight() { }
+
+    public Flight(string fN, string o, string d, datetime et, string s)
+    {
+        FlightNumber = fN;
+        Origin = o;
+        Destination = d;
+        ExpectedTime = et;
+        Status = s;
+    }
+
+    public override ToString()
+    {
+        return "Flight Number: " + FlightNumber + 
+            "\nOrigin: " + Origin + "\nDestination: " + 
+            Destination + "\nExpected Time: " + ExpectedTime + "\nStatus: " + Status;
+    }
+    
+}
