3155ed4d30e4173e964e1f7444b2ce5415cef551
gracechua93 <chua.faithboy@gmail.com>
2025-01-23 22:55:50 +0800

    added LWTTFlight.cs

---
+++ LWTTFlight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class LWTTFlight : Flight
+{
+    public double RequestFee { get; set; }
+
+    public LWTTFlight() : base() { }
+
+    public LWTTFlight(string fN, string o, string d, datetime et, string s, double rF) : base()
+    {
+        RequestFee = rF;
+    }
+
+    public override double CalculateFees()
+    {
+
+    }
+
+    public override string ToString()
+    {
+
+    }
+}
