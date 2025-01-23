25c5941cca078efef4d589f69e795c3b1f26170c
gracechua93 <chua.faithboy@gmail.com>
2025-01-23 22:57:26 +0800

    added DDJBFlight.cs

---
+++ DDJBFlight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class DDJBFlight : Flight
+{
+    public double RequestFee { get; set; }
+
+    public DDJBFlight() : base() { }
+
+    public DDJBFlight(string fN, string o, string d, datetime et, string s, double rF) : base()
+    {
+        RequestFee = rF;
+    }
+
+    public override double CalculateFees()
+    {
+
+    }
+
+    public override string ToString()
+    {
+
+    }
+}
