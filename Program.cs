a53a133d47bd8338c92a9e11407739249e7ceb7e
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 15:19:21 +0800

    added load boardinggate.csv

--- Program.cs
+++ Program.cs
@@ -47,17 +47,46 @@ void LoadFlights(Dictionary<string, Flight> flightDict)
             airlineDict[aircode] = airline;
         }
     }
+    Console.WriteLine("{0,-15} {1,-20} {2,-23} {3,-20} {4,-15}",
+                "Flight Number", "Airline Number", "Origin", "Destination", "Expected Departure/Arrival Time");
     foreach (KeyValuePair<string, Airline> a in airlineDict)
     {
 
         foreach (KeyValuePair<string, Flight> f in flightDict)
         {
-            Console.WriteLine("{0,-15} {1,-20} {2,-23} {3,-20} {4,-15}",
-                "Flight Number", "Airline Number", "Origin", "Destination", "Expected Departure/Arrival Time");
+
             Console.WriteLine("{0,-15} {1,-20} {2,-23} {3,-20} {4,-15}",
                 f.Value.FlightNumber, a.Value.Name, f.Value.Origin, f.Value.Destination, f.Value.ExpectedTime);
         }
     }
+}
 
+LoadBoardingGates(boardingGateDict);
+void LoadBoardingGates(Dictionary<string, BoardingGate> bgDict)
+{
+    using (StreamReader sr = new StreamReader("boardinggates.csv"))
+    {
+        string s = sr.ReadLine();
+
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] data = s.Split(',');
+            string bGate = data[0];
+            bool isDDJB = bool.Parse(data[1]);
+            bool isCFFT = bool.Parse(data[2]);
+            bool isLWTT = bool.Parse(data[3]);
+
+            // Add to the flight dictionary
+            BoardingGate boardingGate = new BoardingGate(bGate, isDDJB, isCFFT, isLWTT);
+            bgDict[bGate] = boardingGate;
+        }
+    }
+    Console.WriteLine("{0,-15} {1,-20} {2,-20} {3,-20}",
+            "Gate Number", "DDJB", "CFFT", "LWTT");
+    foreach (KeyValuePair<string, BoardingGate> bg in bgDict)
+    {
+        Console.WriteLine("{0,-15} {1,-20} {2,-20} {3,-20}",
+            bg.Value.GateName, bg.Value.SupportsDDJB, bg.Value.SupportsCFFT, bg.Value.SupportsLWTT);
+    }
 }
 
